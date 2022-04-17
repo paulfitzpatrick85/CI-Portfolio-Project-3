2295490b4a47653d7a963633830a1c9caf199288
paulfitzpatrick85 <fitzer99@gmail.com>
2022-04-17 13:37:11 +0000

    intial commit

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#GET RANDOM WORD:
+
+import random
+
+words = ["roof", "five", "four", "ball", "room"]
+
+word = random.choice(words).lower()
+print(word)
+
+
+#FOR LETTER IN GUESS WORD:
+
+word ="banana"
+if "i" in word:
+  print("yes")
+else:
+  print(r"""  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========""")
+
+
+
+
